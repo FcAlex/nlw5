a6305e2b20473fd41bf30932922463494c0683e4
Alex Sousa <contato.fcoalex@gmail.com>
2021-04-22 13:48:35 +0000

    add end of day 03

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ import { GetStaticProps } from 'next'
 import Image from 'next/image'
 import { api } from '../services/api'
 import ptBR from 'date-fns/locale/pt-BR'
+import Link from 'next/link'
 import {format, parseISO} from 'date-fns'
 import { convertDurationToTimeString } from '../utils/convertDurationToTimeString'
 import styles from './home.module.scss'
@@ -46,7 +47,9 @@ export default function Home({lastestEpisodes, allEpisodes}: HomeProps) {
                                 />
 
                                 <div className={styles.episodeDetails}>
-                                    <a href="">{episode.title}</a>
+                                    <Link href={`/episodes/${episode.id}`}>
+                                        <a>{episode.title}</a>
+                                    </Link>
                                     <p>{episode.members}</p>
                                     <span>{episode.publishedAt}</span>
                                     <span>{episode.durationAsString}</span>
@@ -61,7 +64,50 @@ export default function Home({lastestEpisodes, allEpisodes}: HomeProps) {
                 </ul>
             </section>
 
-            <section className={styles.allEpisodes}></section>
+            <section className={styles.allEpisodes}>
+                <h2>Todos episódios</h2>
+
+                <table cellSpacing={0}>
+                    <thead>
+                        <th></th>
+                        <th>Podcast</th>
+                        <th>Integrantes</th>
+                        <th>Data</th>
+                        <th>Duração</th>
+                        <th></th>
+                    </thead>
+                    <tbody>
+                        {allEpisodes.map(episode => {
+                            return (
+                                <tr key={episode.id}>
+                                    <td style={{width: 72}}>
+                                        <Image 
+                                            width={120}
+                                            height={120}
+                                            src={episode.thumbnail}
+                                            alt={episode.title}
+                                            objectFit="cover"
+                                        />
+                                    </td>
+                                    <td>
+                                        <Link href={`/episodes/${episode.id}`}>
+                                            <a>{episode.title}</a>
+                                        </Link>
+                                    </td>
+                                    <td>{episode.members}</td>
+                                    <td style={{width: 100}}>{episode.publishedAt}</td>
+                                    <td>{episode.durationAsString}</td>
+                                    <td>
+                                        <button type="button">
+                                            <img src="/play-green.svg" alt="Tocar episódio"/>
+                                        </button>
+                                    </td>
+                                </tr>
+                            )
+                        })}
+                    </tbody>
+                </table>
+            </section>
         </div>
     )
 }
@@ -80,8 +126,8 @@ export const getStaticProps: GetStaticProps = async () => {
             id: episode.id,
             title: episode.title,
             thumbnail: episode.thumbnail,
-            member: episode.thumbnail,
-            publishedAt: format(parseISO(episode.published_at), 'd MM yy', {locale: ptBR}),
+            members: episode.members,
+            publishedAt: format(parseISO(episode.published_at), 'd MMM yy', {locale: ptBR}),
             duration: Number(episode.file.duration),
             durationAsString: convertDurationToTimeString(Number(episode.file.duration)),
             description: episode.description,
